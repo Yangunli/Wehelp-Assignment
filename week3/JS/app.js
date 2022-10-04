e78928c2cac4f199e98caeebb59c643414fb00a6
Yangunli <yangun410@gmail.com>
2022-10-04 20:49:24 +0800

    week3-3

--- week3/JS/app.js
+++ week3/JS/app.js
@@ -1,25 +1,44 @@
-const titleContainer = document.querySelector(".title-container")
-const getData = fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json")
-.then(function (response) {
-    return response.json()
-})
-.then(function (myJson) {
-    let data = myJson.result.results
-    for (let i = 0; i < 10; i++) {
+const container = document.querySelector(".container");
+const promotionContainer = document.createElement("div")
+const titleContainer =  document.createElement("div")
+promotionContainer.setAttribute("class","promotion-container")
+titleContainer.setAttribute("class","title-container")
+container.appendChild(promotionContainer)
+container.appendChild(titleContainer)
+let loadBtn = document.createElement("button")
+container.appendChild(loadBtn)
+loadBtn.setAttribute("class", "loadBtn")
+loadBtn.textContent = "Load More"
+
+// first fetch data
+const getData =fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function(response) {
+    return response.json();
+  }).then(function(myJson) {
+    let data=(myJson.result.results)
+    for(let i=0; i<10; i++){
+      let view = data[i]
+      let imgUrl=`htt${view.file.split('htt')[1]}`
+      let stitle =view.stitle
+      let viewImg = document.createElement("img")
+      let viewTitle = document.createElement("p")
+      if(i <2){
+        const promotion = document.createElement("div");
+        promotion.setAttribute("class", "promotion");
+        document.querySelector(".promotion-container").appendChild(promotion);
+        promotion.appendChild(viewImg)
+        promotion.appendChild(viewTitle)
+        viewImg.setAttribute("src", imgUrl)
+        viewTitle.textContent = stitle       
+      }else{
         const title = document.createElement("div");
         title.setAttribute("class", "title");
         document.querySelector(".title-container").appendChild(title);
-        let view = data[i];
-        let imgUrl = `htt${view.file.split("htt")[1]}`;
-        let stitle = view.stitle;
-        let viewImg = document.createElement("img");
-        let viewTitle = document.createElement("p");
-        let star= document.createElement("div")
-        star.setAttribute("class","star-icons")
-        title.appendChild(star)
-        title.appendChild(viewImg);
-        title.appendChild(viewTitle);
-        viewImg.setAttribute("src", imgUrl);
-        viewTitle.textContent = stitle;
+        title.appendChild(viewImg)
+        title.appendChild(viewTitle)
+        viewImg.setAttribute("src", imgUrl)
+        viewTitle.textContent = stitle       
+      }  
     }
-});
+  }
+)
+ 
