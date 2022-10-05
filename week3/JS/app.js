e731ef36fc03c9c45449ce59099c7d68c7892b82
Yangunli <yangun410@gmail.com>
2022-10-05 19:24:12 +0800

    Add loadBtn

--- week3/JS/app.js
+++ week3/JS/app.js
@@ -5,6 +5,13 @@ promotionContainer.setAttribute("class","promotion-container")
 titleContainer.setAttribute("class","title-container")
 container.appendChild(promotionContainer)
 container.appendChild(titleContainer)
+const loadBtn = document.createElement("button")
+loadBtn.setAttribute("class", "loadBtn")
+loadBtn.textContent="Load More"
+container.appendChild(loadBtn)
+
+
+
 
 // first fetch data
 const getData =fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function(response) {
@@ -38,3 +45,30 @@ const getData =fetch("https://padax.github.io/taipei-day-trip-resources/taipei-a
   }
 )
  
+
+function loadMore(){
+  fetch("https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json").then(function(response) {
+    return response.json();
+  }).then(function(myJson) {
+    let data=(myJson.result.results)
+    for(let i=10; i<data.length; i++){
+      let view = data[i]
+      let imgUrl=`htt${view.file.split('htt')[1]}`
+      let stitle =view.stitle
+      let viewImg = document.createElement("img")
+      let viewTitle = document.createElement("p")
+      const title = document.createElement("div");
+      title.setAttribute("class", "title");
+      document.querySelector(".title-container").appendChild(title);
+      title.appendChild(viewImg)
+      title.appendChild(viewTitle)
+      viewImg.setAttribute("src", imgUrl)
+      viewTitle.textContent = stitle       
+    }
+   
+  }
+  
+)
+console.log("hi")
+}
+loadBtn.addEventListener("click", loadMore)
