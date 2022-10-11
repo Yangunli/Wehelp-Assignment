ad6773a979e5bcc79df8e36eccfbe92c518494ef
Yangunli <yangun410@gmail.com>
2022-10-11 21:01:18 +0800

    Update

--- week4/app.py
+++ week4/app.py
@@ -9,26 +9,27 @@ def index():
     return render_template('index.html')
 
 @app.route("/signin", methods=["POST"])
-def signIn():
+def signIn(): 
     name=request.form["name"]
     password=request.form["password"]
-   
-    #存進session
-    session["username"]=name
-
     if name=="test" and password=="test":
+        #存進session
+        session["username"]=name
         return redirect(url_for("member")) 
     elif name=="" or password=="":  
         return redirect(url_for("error",message="請輸入帳號、密碼"))
     else:
-        return redirect(url_for("error"),message="帳號、或密碼輸入錯誤")
+        return redirect(url_for("error",message="帳號、或密碼輸入錯誤"))
+
 
 
 @app.route("/member")
 def member():
-    name=session["username"]
-    if name=="": return redirect(url_for("index"))
-    return render_template("signin.html", name=name)
+    if "username" in session:
+        name=session["username"]
+        if name=="": return redirect(url_for("index"))
+        return render_template("signin.html", name=name)
+    else: return redirect(url_for("index"))
 
 @app.route("/signout")
 def signOut():
